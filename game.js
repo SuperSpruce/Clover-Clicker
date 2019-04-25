51f774372a233a79adaf265a2564b37cf2009f8f
FallingMountain <44987568+FallingMountain@users.noreply.github.com>
2019-04-25 16:41:47 -0500

    Update game.js

--- game.js
+++ game.js
@@ -34,7 +34,7 @@ function buyC1(){
     document.getElementById('Clover1Cost').innerHTML = nextC1C;  //updates the One Leaf Clover cost for the user
 };
 
-window.setInterval(function() {
+setInterval(function() {
 	MakeFlowersOutOfThinAir(game.state.Clover1)}, 1000);
 
 function buyC3(){

6ef6ba27360063e16e2c2b9a9f99f8cf68eb7b70
SuperSpruce <48099460+SuperSpruce@users.noreply.github.com>
2019-11-22 21:54:11 -0500

    Update game.js

--- game.js
+++ game.js
@@ -78,6 +78,7 @@ var game = {
 };
 
 var AverageFlowerPerSecond;
+var FlowerPerClick;
 
 function MakeFlowersOutOfThinAir(C0){
     game.state.flower += C0;
@@ -136,8 +137,13 @@ function UpdateAverageFlowerPerSecond(){
 	document.getElementById('AverageFlowerPerSecond').innerHTML = AverageFlowerPerSecond;
 };
 
+function UpdateFlowerPerClick(){
+	document.getElementById('FlowerPerClick').innerHTML = FlowerPerClick;
+};
+
 setInterval(function() {
-    AverageFlowerPerSecond = Math.round(game.state.Clover1 * game.state.Clover1Mult + (16.6666667 * game.state.Clover3 * game.state.Clover3Mult) + 1500 * game.state.Clover4 * game.state.Clover4Mult)
+    AverageFlowerPerSecond = Math.round(game.state.Clover1 * game.state.Clover1Mult + (16.6666667 * game.state.Clover3 * game.state.Clover3Mult) + 1500 * game.state.Clover4 * game.state.Clover4Mult);
+    FlowerPerClick = tap;
     UpdateAverageFlowerPerSecond()
     for (let i of upgList) i.updateButton();
     game.state.Clover1Mult = 1;
